c01cf828c8c5259b7c692d7c7bb63f0ac46e9ca1
centek134 <centek134@wp.pl>
2021-07-18 09:12:02 +0200

    Few changes - we get our img url by cutting it from url and then we provide this url to <img/> - before displaying we are checking if we got our img url passes in browser url - To style we are using styled components

--- src/GeneratedMeme/GeneratedMeme.js
+++ src/GeneratedMeme/GeneratedMeme.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import {useHistory, useLocation} from 'react-router-dom';
+import React from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import styled from "styled-components";
 
+const ContainerMeme = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 
 const GeneratedMeme = () => {
+  const history = useHistory();
+  const location = useLocation();
+  let trimmed = location.search.slice(location.search.indexOf("=") + 1);
+  const url = new URLSearchParams(location.search).get("url");
+  console.log("trim me senpai", trimmed);
+  console.log(history);
 
-    const history = useHistory();
-    const location = useLocation();
-    let trimmed = location.search.slice(location.search.indexOf("=") + 1);
-    console.log("trim me senpai",trimmed)
-    console.log(history);
-
-
-    return (
-        <div>
-            <button onClick = {() => history.push('/')}> go back</button>
-            <img src={trimmed}/>            
-        </div>
-    )
+  return (
+    <div>
+      {url ? (
+        <ContainerMeme>
+          <img src={trimmed} alt="I'ts your generated meme" />
+          <button onClick={() => history.push("/")}> go back</button>
+        </ContainerMeme>
+      ) : null}
+    </div>
+  );
 };
 
 export default GeneratedMeme;
