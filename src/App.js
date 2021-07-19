06abc8518606a2828fca99be19a34618b8ba36f4
centek134 <centek134@wp.pl>
2021-07-19 11:34:27 +0200

    Restructuring app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import { Meme } from './Meme/Meme';
-import styled from 'styled-components';
+import React from "react";
+import { Meme } from "./Meme/Meme";
+import GeneratedMeme from "./GeneratedMeme/GeneratedMeme";
+import styled from "styled-components";
+import { Switch, Route } from "react-router-dom";
 
-
-  const MyApp = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  `;
+const MyApp = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const App = () => {
-
   return (
     <MyApp>
-      <Meme></Meme>
+      <Switch>
+        <Route exact path="/">
+          <Meme />
+        </Route>
+        <Route path = "/generated">
+          <GeneratedMeme/>
+        </Route>
+      </Switch>
     </MyApp>
   );
-}
+};
 
 export default App;
-
-
-
